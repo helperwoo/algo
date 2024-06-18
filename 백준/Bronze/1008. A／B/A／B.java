1b5ed5b78c51287f18bcfb31ded07e9f4535aabb
helperwoo <helperwoo.ko@gmail.com>
2024-06-18 17:27:13 +0900

    [Bronze V] Title: A/B, Time: 124 ms, Memory: 14256 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        System.out.print(Double.parseDouble(input[0]) / Double.parseDouble((input[1])));
+    }
+}
