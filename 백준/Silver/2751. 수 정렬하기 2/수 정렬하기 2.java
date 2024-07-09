a0210b0070114cbd8d420d21bf533a096186428b
helperwoo <helperwoo.ko@gmail.com>
2024-07-09 09:18:25 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1532 ms, Memory: 141268 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -7,46 +7,19 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int length = Integer.parseInt(br.readLine());
-        int[] arr = new int[length];
+        ArrayList<Integer> list = new ArrayList<>();
         for (int i=0; i<length; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
+            list.add(Integer.parseInt(br.readLine()));
         }
+        
+        Collections.sort(list);
 
-        int[] result = merge(arr);
-
-        for (int num : result) {
+        for (int num : list) {
             bw.write(Integer.toString(num));
             bw.newLine();
         }
 
         bw.flush();
         bw.close();
-    }
-
-    public static int[] merge(int[] arr) {
-        int[] result = new int[arr.length];
-        int[] first = Arrays.copyOfRange(arr, 0, arr.length/2);
-        int[] second = Arrays.copyOfRange(arr, arr.length/2, arr.length);
-        Arrays.sort(second);
-        int a = 0, b = 0;
-
-        if (first.length >= 2) {
-            first = merge(first);
-            second = merge(second);
-        }
-
-        for (int i=0; i<arr.length; i++) {
-            if (a == first.length) {
-                result[i] = second[b++];
-            } else if (b == second.length) {
-                result[i] = first[a++];
-            } else if (first[a] > second[b]) {
-                result[i] = second[b++];
-            } else if (first[a] < second[b]) {
-                result[i] = first[a++];
-            }
-        }
-
-        return result;
     }
 }
