2d68bed758854524a8e5f02766ff6fd67706677d
고재우 <helperwoo.ko@gmail.com>
2024-12-27 16:53:24 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 124 ms, Memory: 15848 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
@@ -1,24 +1,23 @@
 import java.io.*;
 import java.util.*;
-import java.util.stream.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		String S = br.readLine();
 
-        String input = br.readLine();
-        List<Integer> list = IntStream.rangeClosed(97, 122).boxed().collect(Collectors.toList());
+		int[] arr = new int[26];
+		for (int i=0; i<arr.length; i++) {
+			for (char ch : S.toCharArray()) {
+				if ((int) ch == i + 97) {
+					arr[i] += 1;
+				}
+			}
+			bw.write(arr[i] + " ");
+		}
 
-        for (int num : list) {
-            int count = 0;
-            for (char ch : input.toCharArray()) {
-                if ((int) ch == num) count++;
-            }
-            bw.write(count + " ");
-        }
-
-        bw.flush();
-        bw.close();
-    }
+		bw.flush();
+		bw.close();
+	}
 }
